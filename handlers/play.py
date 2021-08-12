f0076d9bc02a43f27b66a10407e5ecb7b4ea3e64
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-08-12 07:06:12 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -290,7 +290,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption="**🎧 𝑆𝑜𝑛𝑔:-** {}\n**⏳ 𝐷𝑢𝑟𝑎𝑡𝑖𝑜𝑛:-** {} min\n**👤 𝐴𝑑𝑑𝑒𝑑 𝐵𝑦:-** {}\n\n**▶️ 𝑁𝑜𝑤 𝑃𝑙𝑎𝑦𝑖𝑛𝑔 𝑎𝑡 `{}`...**".format(
+        caption="**🎧 𝑆𝑜𝑛𝑔:-** {}\n**⏳ 𝐷𝑢𝑟𝑎𝑡𝑖𝑜𝑛:-** {} min\n**👤 𝐴𝑑𝑑𝑒𝑑 𝐵𝑦:-** {}\n\n**▶️ 𝑁𝑜𝑤 𝑃𝑙𝑎𝑦𝑖𝑛𝑔 𝑎𝑡 :-** `{}`...".format(
         title, duration, message.from_user.mention(), message.chat.title
         ), )
         os.remove("final.png")
